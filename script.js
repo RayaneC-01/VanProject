1e783d1c5586891e06040f3ff98fa140e880fd20
Rayane <rchaabane20@gmail.com>
2024-05-22 01:09:01 +0200

    CamperVan conversion

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.btn');
-  
+
     buttons.forEach(button => {
-      button.addEventListener('click', function () {
-        const vehicleName = this.parentNode.querySelector('.card-title').innerText;
-        const vehicleDescription = this.parentNode.querySelector('.card-text').innerText;
-        Swal.fire({
-          title: vehicleName,
-          text: vehicleDescription,
-          icon: 'info',
-          html: `
-            <a href="https://www.example.com/${vehicleName.toLowerCase().replace(/ /g, '_')}_details.html">En savoir plus sur le ${vehicleName}</a>
-          `,
-          confirmButtonText: 'OK'
+        button.addEventListener('click', function () {
+            const vehicleName = this.parentNode.querySelector('.card-title').innerText;
+            const vehicleDescription = this.parentNode.querySelector('.card-text').innerText;
+            Swal.fire({
+                title: vehicleName,
+                text: vehicleDescription,
+                icon: 'info',
+                button: 'OK'
+            });
         });
-      });
     });
-  });
-  
+}); 
