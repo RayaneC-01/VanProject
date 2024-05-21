5d3b783a7904512006f0abaf265988cdefe5d7ed
Rayane <rchaabane20@gmail.com>
2024-05-22 00:40:02 +0200

    CamperVan conversion

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('.btn');
-
+  
     buttons.forEach(button => {
-        button.addEventListener('click', function () {
-            const vehicleName = this.parentNode.querySelector('.card-title').innerText;
-            const vehicleDescription = this.parentNode.querySelector('.card-text').innerText;
-            Swal.fire({
-                title: vehicleName,
-                text: vehicleDescription,
-                icon: 'info',
-                html: `
-                You can use <b>bold text</b>,
-                <a href="#">links</a>,
-                and other HTML tags
-              `,
-                button: 'OK'
-            })
+      button.addEventListener('click', function () {
+        const vehicleName = this.parentNode.querySelector('.card-title').innerText;
+        const vehicleDescription = this.parentNode.querySelector('.card-text').innerText;
+        Swal.fire({
+          title: vehicleName,
+          text: vehicleDescription,
+          icon: 'info',
+          html: `
+            <a href="https://www.example.com/${vehicleName.toLowerCase().replace(/ /g, '_')}_details.html">En savoir plus sur le ${vehicleName}</a>
+          `,
+          confirmButtonText: 'OK'
         });
+      });
     });
-}); 
+  });
+  
