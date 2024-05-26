307bdb741e0e9438ccd9519209b3f952ea9b2f59
Rayane <rchaabane20@gmail.com>
2024-05-26 23:59:07 +0200

    Traitement contact formulaire

--- contact_processing.php
+++ contact_processing.php
@@ -1,3 +1,9 @@
 <?php
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['nameForm'], $_POST['emailForm'], $_POST['messageForm'])) {
+    $name_user = $_POST['nameForm']; // Récupérer le nom depuis le formulaire
+    $email_user = $_POST['passwemailFormord']; // Récupérer l'email
+    $message_user = $_POST['messageForm'];
 
+}
 ?>
