413994267c8c0533775f9c20e6fca34c773a6245
Rayane <rchaabane20@gmail.com>
2024-05-28 23:41:21 +0200

    Connection DB en PostGreSQL

--- PHP/connection_db.php
+++ PHP/connection_db.php
@@ -1,18 +1,15 @@
 <?php
-// Informations de connexion à la base de données
-$host = 'localhost';
-$port = '5432';         // Le port PostgreSQL
-$dbname = 'vehicules';  // Le nom de la base de données
-$dbuser = 'postgres'; // Remplacez par votre nom d'utilisateur PostgreSQL
-$dbpass = '123456';
+$host = 'localhost'; // Adresse de votre serveur PostgreSQL
+$dbname = 'vehicules'; // Nom de votre base de données
+$dbuser = 'postgres'; // Votre nom d'utilisateur PostgreSQL
+$dbpass = '123456'; // Votre mot de passe PostgreSQL
 
-// Connexion à la base de données
 try {
-    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname;";
-    $pdo = new PDO($dsn, $dbuser, $dbpass, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    echo "Connexion réussie!";
+    $dsn = "pgsql:host=$host;dbname=$dbname";
+    $pdo = new PDO($dsn, $dbuser, $dbpass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
 } catch (PDOException $e) {
-    echo "Erreur de connexion : " . $e->getMessage();
-    exit();
+    die('Connexion échouée : ' . $e->getMessage());
 }
 ?>
