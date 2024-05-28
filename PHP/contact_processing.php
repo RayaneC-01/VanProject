96cfd6440f0053c7b6657b8bf9f9763ba549955b
Rayane <rchaabane20@gmail.com>
2024-05-28 22:15:49 +0200

    traitement d'envoi de message

--- PHP/contact_processing.php
+++ PHP/contact_processing.php
@@ -1,15 +1,35 @@
 <?php
 session_start();
+include 'connection_db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Validation des données
-    $name = htmlspecialchars(trim($_POST['nameForm']));
-    $email = htmlspecialchars(trim($_POST['emailForm']));
-    $message = htmlspecialchars(trim($_POST['messageForm']));
+    // Validation et sécurisation des données
+    $name = htmlentities(trim($_POST['nameForm']), ENT_QUOTES, 'UTF-8');
+    $first_name = htmlentities(trim($_POST['first_name']), ENT_QUOTES, 'UTF-8');
+    $email = htmlentities(trim($_POST['emailForm']), ENT_QUOTES, 'UTF-8');
+    $message = htmlentities(trim($_POST['messageForm']), ENT_QUOTES, 'UTF-8');
 
     if (!empty($name) && filter_var($email, FILTER_VALIDATE_EMAIL) && !empty($message)) {
-        // Vous pouvez ici envoyer un email ou enregistrer le message dans une base de données
-        $_SESSION['message_success'] = "Votre message a été envoyé avec succès.";
+        try {
+            // Préparer la requête d'insertion
+            $query = "INSERT INTO messages (nom,first_name, email, message) VALUES (:name,:first_name, :email, :message)";
+            $stmt = $pdo->prepare($query);
+
+            // Lier les paramètres
+            $stmt->bindParam(':name', $name);
+            $stmt->bindParam(':first_name', $first_name);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':message', $message);
+
+            // Exécuter la requête
+            if ($stmt->execute()) {
+                $_SESSION['message_success'] = "Votre message a été envoyé avec succès.";
+            } else {
+                $_SESSION['message_error'] = "Une erreur est survenue lors de l'envoi de votre message.";
+            }
+        } catch (PDOException $e) {
+            $_SESSION['message_error'] = "Erreur de base de données : " . $e->getMessage();
+        }
     } else {
         $_SESSION['message_error'] = "Veuillez remplir tous les champs correctement.";
     }
