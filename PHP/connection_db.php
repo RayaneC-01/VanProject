27f3ef313a1e20b7679f4a236f428bbf988acf01
Rayane <rchaabane20@gmail.com>
2024-05-29 00:22:28 +0200

    Connection DB en PostGreSQL

--- PHP/connection_db.php
+++ PHP/connection_db.php
@@ -1,15 +1,23 @@
 <?php
-$host = 'localhost'; // Adresse de votre serveur PostgreSQL
-$dbname = 'vehicules'; // Nom de votre base de données
-$dbuser = 'postgres'; // Votre nom d'utilisateur PostgreSQL
-$dbpass = '123456'; // Votre mot de passe PostgreSQL
+// Adresse de votre serveur PostgreSQL
+$host = 'localhost';
+// Nom de votre base de données
+$dbname = 'vehicules';
+// nom d'utilisateur PostgreSQL
+$dbuser = 'postgres';
+// mot de passe postGreSQL
+$dbpass = '123456';
 
 try {
+    // Création de la chaîne de connexion PDO avec les informations fournies
     $dsn = "pgsql:host=$host;dbname=$dbname";
+    // Tentative de connexion à la base de données avec PDO
     $pdo = new PDO($dsn, $dbuser, $dbpass, [
+        // Configuration des options de PDO pour afficher les erreurs en cas de problème
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     ]);
 } catch (PDOException $e) {
+    // Gestion des erreurs de connexion : affichage du message d'erreur
     die('Connexion échouée : ' . $e->getMessage());
 }
 ?>
