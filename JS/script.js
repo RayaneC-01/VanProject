3d5e7bc90136e5f9524c2b2b6b2644679880e3cc
Rayane <rchaabane20@gmail.com>
2024-05-29 19:44:04 +0200

    FIchier JS affichage des vehicules

--- JS/script.js
+++ JS/script.js
@@ -33,101 +33,138 @@ document.addEventListener('DOMContentLoaded', function () {
             link: 'https://www.ford.fr/utilitaires-et-pick-up/nouveau-ranger'
         },
         {
+            type: 'pickup',
             name: 'Toyota Hilux',
             description: 'Description du Toyota Hilux.',
             image: 'https://scene7.toyota.eu/is/image/toyotaeurope/toyota-hilux-2020-gallery-03-full_tcm-18-2017452?wid=1280&fit=fit,1&ts=0&resMode=sharp2&op_usm=1.75,0.3,2,0',
             link: 'https://www.toyota.fr/vehicules-neufs/hilux'
         },
         {
+            type: 'pickup',
             name: 'Nissan Navara',
             description: 'Description du Nissan Navara.',
             image: 'https://images.caradisiac.com/logos-ref/modele/modele--nissan-navara-3/S7-modele--nissan-navara-3.jpg',
             link: 'https://www.caradisiac.com/modele--nissan-navara-3/'
         },
         {
+            type: 'pickup',
             name: 'Volkswagen Amarok',
             description: 'Description du Volkswagen Amarok.',
             image: 'https://www.automobile-magazine.fr/asset/cms/840x394/6353/config/6047/ce-restylage-de-lamarok-1-ne-transfigure-pas-vraiment-la-plastique-musclee-de-ce-beau-bebe.jpg',
             link: 'https://www.volkswagen-nutzfahrzeuge.de/de/modelle/amarok.html'
         },
         {
+            type: 'pickup',
             name: 'Mercedes-Benz X-Class',
             description: 'Description du Mercedes-Benz X-Class.',
             image: 'https://images.clickdealer.co.uk/vehicles/5254/5254336/large1/124197960.jpg',
             link: 'https://www.mercedes-benz.fr/passengercars/mercedes-benz-cars/models/x-class/x-class-pickup/explore.html'
         },
         {
+            type: 'pickup',
             name: 'Chevrolet Colorado',
             description: 'Description du Chevrolet Colorado.',
             image: 'https://img.sm360.ca/images/article/groupe-dallaire/106587//2023-colorado-gal-ext-011667230712658.jpg',
             link: 'https://www.chevrolet.com/trucks/colorado'
         },
         {
+            type: 'pickup',
             name: 'Ford F-150',
             description: 'Description du Ford F-150.',
             image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMoCeaM4_fm4LSMptwstILyMdGkNxGULJYXXlcxmHN8A&s',
             link: 'https://www.ford.com/trucks/f150/'
         },
         {
+            type: 'pickup',
             name: 'Jeep Gladiator',
             description: 'Description du Jeep Gladiator.',
             image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0J2BSUw1--u3WYTFvnFoTeBBBBn5RYzT36_puMv1bnA&s',
             link: 'https://www.jeep.com/gladiator.html'
         },
         {
+            type: 'pickup',
             name: 'Honda Ridgeline',
             description: 'Description du Honda Ridgeline.',
             image: 'https://hips.hearstapps.com/hmg-prod/images/2024-honda-ridgeline-trailsport-117-660c4ee75bd07.jpg?crop=0.901xw:0.674xh;0.0537xw,0.243xh&resize=1200:*',
             link: 'https://automobiles.honda.com/ridgeline'
         },
         {
+            type: 'pickup',
             name: 'Ford Econoline (E-Series)',
             description: 'Description du Ford Econoline (E-Series).',
             image: 'https://www.contravans.com/wp-content/uploads/2023/10/IMG_E3484-1536x1152.jpg',
             link: 'https://www.contravans.com/ford-econoline-campervan-custom/'
         },
         {
+            type: 'pickup',
             name: 'Mitsubishi L200',
             description: 'Description du Mitsubishi L200.',
             image: 'https://sf2.autoplus.fr/wp-content/uploads/autoplus/2022/12/1-89.jpg',
             link: 'https://www.dna.fr/magazine-automobile/2023/10/16/pick-up-le-nouveau-mitsubishi-l200-deja-distingue'
         },
         {
+            type: 'van',
+
             name: 'Hymer Exsis-i 474', description: 'Description du Hymer Exsis-i 474.', image: 'https://www.campingcarlesite.com/wp-content/uploads/2022/05/2022-Integral-Hymer-Exsis-i-474-07.jpg', link: 'https://www.campingcarlesite.com/essai-camping-car/54177-hymer-exsis-i-474-les-atouts-dun-integral-dans-un-petit-format-664-m'
         },
         {
+            type: 'van',
+
             name: 'Ford Transit 2T', description: 'Description du Ford Transit 2T.', image: 'https://www.vanlifemag.fr/wp-content/uploads/2022/06/dethleffs-590c-001-e1675940818135-768x432.jpg', link: 'https://www.vanlifemag.fr/presentationessais/ford-transit/66425'
         },
         {
+            type: 'van',
+
             name: 'Ford Transit Custom Nugget', description: 'Description du Ford Transit Custom Nugget.', image: 'https://live.dealer-asset.co/images/fr4/product/file/TransitCustomNugget-638489450688005219.jpg?s=2000', link: 'https://www.motor1.com/news/683268/2024-ford-transit-custom-nugget/'
         },
         {
+            type: 'van',
+
             name: 'Mercedes Sprinter', description: 'Description du Mercedes Sprinter.', image: 'https://images.squarespace-cdn.com/content/v1/57ef0117579fb34c2451c73b/1610141260192-BV2N87XMRPF2QEFH5EMY/Copy+of+Toops_Sprinter+144-26.jpg?format=1500w', link: 'https://vanlifecustoms.com/chads144sprinter'
         },
         {
+            type: 'van',
+
             name: 'Volkswagen Transporter', description: 'Description du Volkswagen Transporter.', image: 'https://www.ecowagon.co.uk/wp-content/uploads/2020/04/Custom-VW-T6-Specialists-UK.jpg', link: 'https://www.ecowagon.co.uk/custom-van-options/'
         },
         {
+            type: 'van',
+
             name: 'Fiat Ducato', description: 'Description du Fiat Ducato.', image: 'https://www.fiatcamper.com/content/dam/camper/cross/product/fiat-ducato-camper-van/modular-dimensions/cover-2.jpg', link: 'https://www.fiatcamper.com/fr/produit/Fiat-ducato-camper-van'
         },
         {
+            type: 'van',
+
             name: 'Renault Master', description: 'Description du Renault Master.', image: 'https://www.vanlifemag.fr/wp-content/uploads/2023/07/font-vendome-leader-camp-elegance1.jpg', link: 'https://www.vanlifemag.fr/presentationessais/font-vendome-nos-photos-exclusives-des-nouveaux-camper-vans-elegance/69261'
         },
         {
+            type: 'van',
+
             name: 'Peugeot Boxer', description: 'Description du Peugeot Boxer.', image: 'https://www.transitcenter.fr/graphics/new_brands/Boxerglowne.jpg', link: 'https://www.transitcenter.fr/peugeot-boxer.php'
         },
 
         {
+            type: 'van',
+
             name: 'Nissan NV300',
             description: 'Description du Nissan NV300',
             image: 'https://assets.quirkycampers.com/uk/wp-content/uploads/2022/04/20220416_142859-1-1024x768.jpg',
             link: 'https://www.quirkycampers.com/uk/for-sale/converted-nissan-nv300/'
         },
         {
+            type: 'van',
             name: 'Ford Econoline',
             description: 'Description du Nissan NV300 Ford Ecoline',
             image: 'https://www.contravans.com/wp-content/uploads/2023/10/IMG_E3484-1536x1152.jpg',
             link: 'https://www.contravans.com/ford-econoline-campervan-custom/'
+        },
+
+        {
+            type: 'sport',
+            name: 'Chevrolet corvette Z06 ',
+            description: 'Description de la Corvette Z06',
+            image: 'https://images.caradisiac.com/images/0/0/6/3/200063/S0-chevrolet-explique-en-video-comment-roder-la-corvette-z06-740960 jpg',
+            link: 'https://www.caradisiac.com/chevrolet-explique-en-video-pourquoi-roder-la-corvette-z06-200063.htm'
         }
     ];
 
