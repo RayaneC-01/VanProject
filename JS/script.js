96a3632ee61fc97ea4e26ed37ac838ed95f5c8cb
Rayane <rchaabane20@gmail.com>
2024-06-02 22:02:56 +0200

    Ajout filtre des voitures

--- JS/script.js
+++ JS/script.js
@@ -273,23 +273,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Get the "back to top" button element
-    const scrollToTopButton = document.getElementById('js-top');
-
-    // Define a function that checks the scroll position and sets the button's class name accordingly
-    const scrollFunc = () => {
-        // Get the current scroll position
-        let y = window.scrollY;
-
-        // If the scroll position is greater than 0, set the button's class name to "top-link show"
-        if (y > 0) {
-            scrollToTopButton.className = "top-link show";
-        } else {
-            // Otherwise, set the button's class name to "top-link hide"
-            scrollToTopButton.className = "top-link hide";
-        }
-    };
-
     // Add an event listener to the window object that calls the scrollFunc function whenever the user scrolls
     window.addEventListener("scroll", scrollFunc);
 
@@ -307,3 +290,30 @@ document.addEventListener('DOMContentLoaded', function () {
     // Add an event listener to the "back to top" button that calls the scrollToTop function whenever the button is clicked
     scrollToTopButton.addEventListener("click", scrollToTop);
 });
+
+// Fonction qui est appelée lorsque l'utilisateur fait défiler la page
+window.onscroll = function () {
+    scrollFunction();
+};
+
+// Fonction qui gère l'affichage du bouton "To Top" en fonction de la position de défilement
+function scrollFunction() {
+    // Récupère l'élément du bouton "To Top" par son ID
+    const toTopButton = document.getElementById("toTopButton");
+
+    // Vérifie si l'utilisateur a défilé vers le bas de plus de 20 pixels
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        // Si oui, affiche le bouton en le stylisant pour qu'il soit visible
+        toTopButton.style.display = "block";
+    } else {
+        // Sinon, cache le bouton
+        toTopButton.style.display = "none";
+    }
+}
+
+// Ajoute un gestionnaire d'événement au bouton "To Top" pour gérer les clics
+document.getElementById("toTopButton").onclick = function () {
+    // Lorsque le bouton est cliqué, fait défiler la page jusqu'en haut
+    document.body.scrollTop = 0; // Pour Safari
+    document.documentElement.scrollTop = 0; // Pour Chrome, Firefox, IE et Opera
+};
